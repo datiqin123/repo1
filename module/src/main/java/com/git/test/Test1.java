4a3255ed76195cbfec35f7c5044cebb256fbfd8c
lwh <2222@qq.com>
2020-03-15 18:06:02 +0800

    2的提交

--- module/src/main/java/com/git/test/Test1.java
+++ module/src/main/java/com/git/test/Test1.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        System.out.println(list);
     }
 
     public void test(){
